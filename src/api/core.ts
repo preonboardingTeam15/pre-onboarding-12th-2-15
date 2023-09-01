59c3180d0cc44566fd5292b3bc65e96af512e879
seongmin <seongm22n@gmail.com>
2023-09-01 18:39:12 +0900

    fix : github api키 삭제

--- src/api/core.ts
+++ src/api/core.ts
@@ -1,8 +1,6 @@
 import { Octokit } from 'octokit'
 
-const octokit = new Octokit({
-  auth: process.env.REACT_APP_GITHUB_API_KEY,
-})
+const octokit = new Octokit()
 
 export const CORE_API = async (apiMethod: string, path: string, params?: object) => {
   return octokit.request(`${apiMethod} ${path}`, {
